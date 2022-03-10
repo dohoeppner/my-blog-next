3bdcf663f24e7b2fa6ac7dfac7262ef5341e0758
Dora Hoeppner <dora.hoeppner@gmail.com>
2022-03-10 16:28:57 +0100

    Change id js and added jest

--- pages/Blog/[id].js
+++ pages/Blog/[id].js
@@ -1,15 +1,13 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { getPostData, getSortedPostsData} from "../../lib/posts";
+import { getPostData, getSortedPostsData } from "../../lib/posts";
 
-export default function BlogPost({post}) {
-  const router = useRouter();
-  const { id } = router.query;
-
+export default function BlogPost({ post }) {
   return (
     <>
       <h1>{post.title}</h1>
+      <time dateTime={post.date}>{post.date}</time>
       <p>Fabulous content</p>
+      <div dangerouslySetInnerHTML={{ __html: post.contentHtml }}></div>
       <button>
         <Link href="/">
           <a>Back to home</a>
@@ -20,17 +18,17 @@ export default function BlogPost({post}) {
 }
 
 export async function getStaticPaths() {
-  const posts = getPostData();
-return {
-  paths: posts.map((post) => ({params: {post.id}})),
-  fallback: false,
-}
-}
+  const posts = getSortedPostsData();
 
+  return {
+    paths: posts.map((post) => ({ params: { id: post.id } })),
+    fallback: false,
+  };
+}
 
 export async function getStaticProps(context) {
   const id = context.params.id;
-  const post = await getPostsData();
+  const post = await getPostData(id);
   return {
     props: {
       post: post,
